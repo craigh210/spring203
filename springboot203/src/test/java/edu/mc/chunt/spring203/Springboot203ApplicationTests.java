ea6b9703fce088f7972bf170fcc7dfa5a2fce469
craigh210 <craighunt210@gmail.com>
2020-06-20 15:25:43 -0400

    Initial changes

---
+++ springboot203/src/test/java/edu/mc/chunt/spring203/Springboot203ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.mc.chunt.spring203;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot203ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
